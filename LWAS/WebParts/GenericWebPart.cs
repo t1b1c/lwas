0a372b69b73e3a0f8d0e44aecc9bc237a37a13bb
t1b1c <tiberiu.craciun@gmail.com>
2017-02-07 23:06:09 +0000

    generic part

---
+++ LWAS/WebParts/GenericWebPart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LWAS.Extensible.Exceptions;
+using LWAS.Extensible.Interfaces.Monitoring;
+
+namespace LWAS.WebParts
+{
+    public class GenericWebPart : BindableWebPart, IReporter
+    {
+        public IMonitor Monitor { get; set; }
+
+        public override void Initialize()
+        {
+            if (null == this.Configuration) throw new MissingProviderException("Configuration");
+            if (null == this.ConfigurationParser) throw new MissingProviderException("Configuration parser");
+            if (null == this.Binder) throw new MissingProviderException("Binder");
+            if (null == base.ValidationManager) throw new MissingProviderException("ValidationManager");
+            if (null == base.ExpressionsManager) throw new MissingProviderException("ExpressionsManager");
+            if (null == this.Monitor) throw new MissingProviderException("Monitor");
+
+            base.Initialize();
+        }
+    }
+}
